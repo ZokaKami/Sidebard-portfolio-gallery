cc2c076b5d52ede2bac781ab9a2a7337402e030d
ZokaKami <ruffy92@live.com>
2024-02-09 19:37:05 +0100

    Update gallery.tsx
    gap/space-y- update

--- components/gallery.tsx
+++ components/gallery.tsx
@@ -7,7 +7,7 @@ import Frog from "./images/frog.jpg";
 export default function Gallery() {
   return (
     <div className="lg:absolute lg:top-2 lg:left-[21%] ">
-      <div className=" px-4 pb-2  lg:flex  space-y-4 lg:space-y-0      lg:w-[100%]     ">
+      <div className=" px-4 pb-2  lg:flex  space-y-4 lg:space-y-0 gap-[20px]     lg:w-[100%]     ">
         <div className="flex flex-col gap-[20px] w-[100%]">
           <img src={Borzoi} className="w-[100%] h-auto object-cover" alt="" />
           <img src={Necro} className="w-[100%] h-auto object-cover" alt="" />
