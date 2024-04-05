6870051140d7529ff02736d20727a83263e54357
ZokaKami <ruffy92@live.com>
2024-04-05 13:18:53 +0200

    x

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,24 @@ export default {
   "./src/**/*.{js,ts,jsx,tsx}",
   ".//components/**/*.{js,jsx,ts,tsx}",],
   theme: {
-    extend: {},
+    extend: {
+      keyframes: {
+        droplet: {
+          '0%': {
+            transform: 'translateY(0) scale(1)',
+          },
+          '50%': {
+            transform: 'translateY(-10px) scale(1.2)',
+          },
+          '100%': {
+            transform: 'translateY(0) scale(1)',
+          },
+        },
+      },
+      animation: {
+        droplet: 'droplet 0.4s ease-in-out infinite',
+      }
+    },
   },
   plugins: [],
 }
