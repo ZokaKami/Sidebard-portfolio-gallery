4fa26774481e0c93b948334c425e3b01eb8331e8
ZokaKami <ruffy92@live.com>
2024-04-05 13:33:59 +0200

    gallery

---
+++ components/PixelSubGallery.js
@@ -0,0 +1,98 @@
+import MysticSword from "./images/mysticlands/MysticSword.png";
+import WinterOne from "./images/mysticlands/winter1.png";
+import WinterTwo from "./images/mysticlands/winter2.png";
+import ForestOne from "./images/mysticlands/forest3.png";
+import ForestTwo from "./images/mysticlands/forest4.png";
+import ForestThree from "./images/mysticlands/forest1.png";
+import ForestFour from "./images/mysticlands/forest2.png";
+import HouseOne  from "./images/mysticlands/house1.png";
+import HouseTwo from "./images/mysticlands/house2.png";
+
+import HutOne  from "./images/mysticlands/hut1.png";
+import HutTwo from "./images/mysticlands/hut2.png";
+import HutThree from "./images/mysticlands/hut3.png";
+import EntranceOne  from "./images/mysticlands/entrance1.png";
+import EntranceTwo from "./images/mysticlands/entrance2.png";
+import EntranceThree from "./images/mysticlands/entrance3.png";
+import CaveOne  from "./images/mysticlands/cave1.png";
+import CaveTwo from "./images/mysticlands/cave2.png";
+import FortressOne  from "./images/mysticlands/fortress1.png";
+import FortressTwo from "./images/mysticlands/fortress2.png";
+
+
+const PixelSubGallery = {
+    MysticSword:{
+        title: "MysticSword"
+    },
+  Winter: {
+    title: "Pixel one",
+    WinterOne: {
+      name: WinterOne,
+      description: "place holder tester place holder testerplace holder testerplace holder tester",
+    },
+    WinterTwo: {
+      name: WinterTwo,
+      description: "place holder tester egilsgilsgilnslingeilns gsi gsgles gnisg nelsg enslg ensgles geilsgn lsgn sli",
+    },
+    
+  },
+  Forest: {
+    title: "Background",
+    ForestOne: {
+      name: ForestOne,
+      description: "place holder tester",
+    },
+    ForestTwo: {
+      name: ForestTwo,
+      description: "place holder tester",
+    },
+    ForestThree: {
+      name: ForestThree,
+      description: "place holder tester",
+    },
+    ForestFour: {
+        name: ForestFour,
+        description: "place holder tester",
+      },
+  },
+  House: {
+    title: "House",
+    HouseOne: {
+      name: HouseOne,
+      description: "place holder tester",
+    },
+    HouseTwo: {
+      name: HouseTwo,
+      description: "place holder tester",
+    },
+   
+  },
+  Hut: {
+    title: "Hut",
+    HutOne: { description: "place holder testerplace holder testerplace holder testerplace holder testerplace holder testerplace holder testerplace holder testerplace holder tester", name: HutOne },
+    HutTwo: { description: "place holder tester", name: HutTwo },
+    HutThree: { description: "place holder tester", name: HutThree },
+    
+  },
+  Entrance: {
+    title: "Entrance",
+    EntranceOne: { description: "place holder tester", name: EntranceOne },
+    EntranceTwo: { description: "place holder tester", name: EntranceTwo },
+    description: "place holder tester",
+    EntranceThree: { description: "place holder tester", name: EntranceThree },
+  },
+  Cave: {
+    title: "Cave",
+    CaveOne: { description: "place holder tester", name: CaveOne },
+    CaveTwo: { description: "place holder tester", name: CaveTwo },
+     
+  },
+  Fortress: {
+    title: "Fortress",
+    FortressOne: { description: "place holder tester", name: FortressOne },
+    FortressTwo: { description: "place holder tester", name: FortressTwo },
+    
+  },
+};
+
+export default PixelSubGallery;
---
+++ components/test.js
@@ -0,0 +1,115 @@
+import AssetsOne from "./images/AssetsOne.png";
+import AssetsTwo from "./images/AssetsTwo.png";
+import AssetsThree from "./images/AssetsThree.png";
+import BackgroundOne from "./images/BackgroundOne.png";
+import BackgroundTwo from "./images/BackgroundTwo.png";
+import BackgroundThree from "./images/BackgroundThree.png";
+ 
+import FieldOne from "./images/FieldOne.png";
+import FieldTwo from "./images/FieldTwo.png";
+import FoilageOne from "./images/FoilageOne.png";
+import FoilageTwo from "./images/FoilageTwo.png";
+import FoilageThree from "./images/FoilageThree.png";
+import FoilageFour from "./images/FoilageFour.png";
+import FoilageFive from "./images/FoilageFive.png";
+import FurnaceOne from "./images/FurnaceOne.png";
+import FurnaceTwo from "./images/FurnaceTwo.png";
+import FurnaceThree from "./images/FurnaceThree.png";
+import TreeOne from "./images/TreeOne.png";
+import TreeTwo from "./images/TreeTwo.png";
+import TreeThree from "./images/TreeThree.png";
+import WoodOne from "./images/WoodOne.png";
+import WoodTwo from "./images/WoodTwo.png";
+import WoodThree from "./images/WoodThree.png";
+
+ const text = {
+ 
+  Assets: {
+    AssetsOne: {
+      name: AssetsOne,
+    },
+    AssetsTwo: {
+      name: AssetsTwo,
+    },
+    AssetsThree: {
+        name: AssetsThree,
+      },
+  },
+  Background:{
+    BackgroundOne: {
+        name: BackgroundOne,
+      },
+      BackgroundTwo: {
+        name: BackgroundTwo,
+      },
+      BackgroundThree: {
+          name: BackgroundThree,
+        },
+  },
+  Field:{
+    FieldOne: {
+        name: FieldOne,
+      },
+      FieldTwo: {
+        name: FieldTwo,
+      },
+     
+  },
+  Foilage:{
+    FoilageOne: {
+        name: FoilageOne,
+      },
+      FoilageTwo: {
+        name: FoilageTwo,
+      },
+      FoilageThree: {
+        name: FoilageThree,
+      },
+      FoilageFour: {
+        name: FoilageFour,
+      },
+      FoilageFive: {
+        name: FoilageFive,
+      },
+     
+  },
+  Furnace:{
+    FurnaceOne: {
+        name: FurnaceOne,
+      },
+      FurnaceTwo: {
+        name: FurnaceTwo,
+      },
+      FurnaceThree: {
+          name: FurnaceThree,
+        },
+  },
+  Tree:{
+    TreeOne: {
+        name: TreeOne,
+      },
+      TreeTwo: {
+        name: TreeTwo,
+      },
+      TreeThree: {
+          name: TreeThree,
+        },
+  },
+  Wood:{
+    WoodOne: {
+        name: WoodOne,
+      },
+      WoodTwo: {
+        name: WoodTwo,
+      },
+      WoodThree: {
+          name: WoodThree,
+        },
+  },
+};
+  
+
+ 
+ 
+export default text;
+ 
